696fdda704c0ca8da335b714dbcc444c4f781841
Themko30 <alfonsodalbenzio5@gmail.com>
2021-03-17 13:11:31 +0100

    Final Version Commit with Google Style

--- src/main/java/Controller/ServletRequestHeaders.java
+++ src/main/java/Controller/ServletRequestHeaders.java
@@ -2,32 +2,37 @@ package Controller;
 
 import Model.HeadersBean;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
-
 @WebServlet(name = "ServletRequestHeaders", value = "/ServletRequestHeaders")
 public class ServletRequestHeaders extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
 
-		ArrayList<String> headerNames = Collections.list(request.getHeaderNames());
-		ArrayList<String> headerValues = new ArrayList<>();
-		for (String headerName : headerNames) {
-			headerValues.add(request.getHeader(headerName));
-		}
-		HeadersBean headersBean = new HeadersBean(headerNames, headerValues);
-		request.setAttribute("headerskey", headersBean);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Results/ShowRequestHeaders.jsp");
-		dispatcher.forward(request, response);
-	}
+    ArrayList<String> headerNames = Collections.list(request.getHeaderNames());
+    ArrayList<String> headerValues = new ArrayList<>();
+    for (String headerName : headerNames) {
+      headerValues.add(request.getHeader(headerName));
+    }
+    HeadersBean headersBean = new HeadersBean(headerNames, headerValues);
+    request.setAttribute("headerskey", headersBean);
+    RequestDispatcher dispatcher =
+        request.getRequestDispatcher("/WEB-INF/Results/ShowRequestHeaders.jsp");
+    dispatcher.forward(request, response);
+  }
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
--- src/main/java/Model/HeadersBean.java
+++ src/main/java/Model/HeadersBean.java
@@ -4,27 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HeadersBean {
-	private List<String> headerNames;
-	private List<String> headerValues;
+  private List<String> headerNames;
+  private List<String> headerValues;
 
-	public HeadersBean(ArrayList<String> headerNames, ArrayList<String> headerValues) {
-		this.headerNames = headerNames;
-		this.headerValues = headerValues;
-	}
+  public HeadersBean(ArrayList<String> headerNames, ArrayList<String> headerValues) {
+    this.headerNames = headerNames;
+    this.headerValues = headerValues;
+  }
 
-	public List<String> getHeaderNames() {
-		return headerNames;
-	}
+  public List<String> getHeaderNames() {
+    return headerNames;
+  }
 
-	public void setHeaderNames(List<String> headerNames) {
-		this.headerNames = headerNames;
-	}
+  public void setHeaderNames(List<String> headerNames) {
+    this.headerNames = headerNames;
+  }
 
-	public List<String> getHeaderValues() {
-		return headerValues;
-	}
+  public List<String> getHeaderValues() {
+    return headerValues;
+  }
 
-	public void setHeaderValues(List<String> headerValues) {
-		this.headerValues = headerValues;
-	}
+  public void setHeaderValues(List<String> headerValues) {
+    this.headerValues = headerValues;
+  }
 }
